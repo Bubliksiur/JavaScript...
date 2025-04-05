089e010cdd78997cf7f27c1f58cb3a60d9da3c09
Bubliks <149240768+Bubliksiur@users.noreply.github.com>
2025-04-06 00:26:57 +0200

    Add files via upload

---
+++ moduly.js
@@ -0,0 +1,13 @@
+// W pierwszym pliku
+
+let a = 5;
+
+// W drugim pliku
+
+console.log(a); // zmienna nie zadeklarowana
+
+// mozemy to polaczyc przez import().........
+
+// enkapsulacja - kapsulacja kodu
+
+// commmon js - byl wykorzystywany w node.js
